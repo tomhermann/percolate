c5bdd5307c87df1064372555e246ecca9943d881
Tom Hermann <tomhermann@gmail.com>
2014-12-27 01:17:10 -0600

    Cleaned up a little duplication. Tom

--- wiifit-sensor/wiifit-sensor.py
+++ wiifit-sensor/wiifit-sensor.py
@@ -23,20 +23,14 @@ def main():
 
    wiimote.rpt_mode = cwiid.RPT_BALANCE | cwiid.RPT_BTN
    wiimote.request_status()
-
    balance_calibration = wiimote.get_balance_cal()
-   named_calibration = { 'right_top': balance_calibration[0],
-                         'right_bottom': balance_calibration[1],
-                         'left_top': balance_calibration[2],
-                         'left_bottom': balance_calibration[3],
-                       }
-
+     
    while wiimote:
       wiimote.request_status()
       payload = {}
       payload['id'] = '1'
       payload['timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime()) #iso8601-esque
-      payload['weight'] = calcweight(wiimote.state['balance'], named_calibration) / 100.0
+      payload['weight'] = calcweight(wiimote.state['balance'], balance_calibration) / 100.0
       
       response = requests.post('http://crazypowerful.com/percolate/pot', data=json.dumps(payload), headers={'content-type' : 'application/json'})
       print "response: (%d), data: %s" % ( response.status_code, payload)
@@ -45,24 +39,26 @@ def main():
    wiimote.close()
    return 0
 
-def calcweight( readings, calibrations ):
-    """
-    Determine the weight of the user on the board in hundredths of a kilogram
-    """
-    weight = 0
-    for sensor in ('right_top', 'right_bottom', 'left_top', 'left_bottom'):
-        reading = readings[sensor]
-        calibration = calibrations[sensor]
+def calcweight( readings, balance_calibration ):
+   """
+   Determine the weight of the user on the board in hundredths of a kilogram
+   """
+   weight = 0
+    
+   sensors = ['right_top', 'right_bottom', 'left_top', 'left_bottom']   
 
-        # 1700 appears to be the step the calibrations are against.
-        # 17kg per sensor is 68kg, 1/2 of the advertised Japanese weight limit.
+   for sensor in sensors:
+      reading = readings[sensor]
+      calibration = balance_calibration[sensors.index(sensor)]
 
-        if reading < calibration[1]:
-            weight += 1700 * (reading - calibration[0]) / (calibration[1] - calibration[0])
-        else:
-            weight += 1700 * (reading - calibration[1]) / (calibration[2] - calibration[1]) + 1700
+      # 1700 appears to be the step the calibrations are against.
+      # 17kg per sensor is 68kg, 1/2 of the advertised Japanese weight limit.
 
-    return weight
+      if(reading < calibration[1]):
+         weight += 1700 * (reading - calibration[0]) / (calibration[1] - calibration[0])
+      else:
+         weight += 1700 * (reading - calibration[1]) / (calibration[2] - calibration[1]) + 1700
+   return weight
 
 if __name__ == "__main__":
    sys.exit(main())

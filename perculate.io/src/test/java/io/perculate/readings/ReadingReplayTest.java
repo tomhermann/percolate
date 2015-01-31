7a2377cc3aec5554cd272f443464463e9d78c9ed
Tom Hermann <tomhermann@gmail.com>
2015-01-31 14:52:03 -0600

    Ignored test, added description.

--- perculate.io/src/test/java/io/perculate/readings/ReadingReplayTest.java
+++ perculate.io/src/test/java/io/perculate/readings/ReadingReplayTest.java
@@ -3,6 +3,7 @@ package io.perculate.readings;
 import java.util.List;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -13,6 +14,19 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 
+/***
+ * This posts weight readings to the endpoint every 500ms (same rate the data was collected at)
+ * Obviously you need to run the web application, then run this 'test' to insert the data.
+ * 
+ * It might be quicker for development to load the data into a data.sql file which Spring will 
+ * load automatically. I did it this way, so when the websocket is hooked up we'll be able to
+ * replay the data and see the webpage respond.
+ * 
+ * Note: this is ignored so it won't break Jenkins
+ * 
+ * @author Tom Hermann
+ */
+@Ignore
 public class ReadingReplayTest {
 	private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	private static List<String> readings;
@@ -31,7 +45,6 @@ public class ReadingReplayTest {
 			pauseForHalfOfSecond(); 
 		}
 	}
-
 
 	private Request createPostRequest(RequestBody body) {
 		return new Request.Builder().url("http://localhost:8080/perculate/readings").post(body).build();

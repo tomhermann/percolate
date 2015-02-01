d924a0ed7233469b70152818445dd4de2745b89d
Tom Hermann <tomhermann@gmail.com>
2015-02-01 10:40:56 -0600

    Added test, removed RepositoryConfig in favor of defining EventHandler as component. Tom

--- perculate.io/src/main/java/io/perculate/readings/ReadingEventHandler.java
+++ perculate.io/src/main/java/io/perculate/readings/ReadingEventHandler.java
@@ -7,17 +7,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 @RepositoryEventHandler(Reading.class)
 public class ReadingEventHandler {
 	private static final Logger logger = LoggerFactory.getLogger(ReadingEventHandler.class);
-
+	private final SimpMessagingTemplate messagingTemplate;
+	
 	@Inject
-	private SimpMessagingTemplate messagingTemplate;
+	public ReadingEventHandler(SimpMessagingTemplate messagingTemplate) {
+		this.messagingTemplate = messagingTemplate;
+	}
 	
 	@HandleAfterCreate
 	public void handleAfterCreate(Reading reading) {
-		logger.info("Created reading: {}" , reading);
 		messagingTemplate.convertAndSend("/topic/readings", reading);
+		logger.debug("Sent -> '/topic/readings': {}" , reading);
 	}
 }
